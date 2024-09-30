ecf620165a0116a9d2a9615106e2964c0c40657f
Vasilii <mr_tomat@mail.ru>
2024-09-30 13:05:49 +0300

    subnet name fix

--- networks.tf
+++ networks.tf
@@ -3,7 +3,7 @@ resource "yandex_vpc_network" "my-network" {
 }
 
 resource "yandex_vpc_subnet" "subnet-a-zone" {
-  name           = "my-subnet"
+  name           = var.subnet_name
   zone           = var.zone_of_availability
   network_id     = yandex_vpc_network.my-network.id
   v4_cidr_blocks = var.v4_cidr_blocks

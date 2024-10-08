c21e29c9f4275af2a6dd3655fb97912ac332b991
Vasilii <mr_tomat@mail.ru>
2024-10-08 17:52:50 +0300

    ansible hosts file generator in .yaml

--- ansible_hosts_generator.tf
+++ ansible_hosts_generator.tf
@@ -1,4 +1,4 @@
-resource "local_file" "ansible_ini_inventory" {
+resource "local_file" "ansible_inventory_ini" {
   content = <<EOF
 [all:vars]
 ansible_user=ubuntu
@@ -8,4 +8,19 @@ ${join("\n", [for instance in yandex_compute_instance.vm :
 "${instance.name} ansible_host=${instance.network_interface.0.nat_ip_address}"])}
 EOF
 filename = "${path.module}/hosts.ini"
+}
+
+resource "local_file" "ansible_inventory_yaml" {
+  content = yamlencode({
+    "all" : {
+      "hosts" : {
+        for instance in yandex_compute_instance.vm :
+        instance.name => {
+          "ansible_host" : instance.network_interface.0.nat_ip_address,
+          "ansible_user" : "ubuntu"
+        }
+      }
+    }
+  })
+  filename = "${path.module}/hosts.yaml"
 }

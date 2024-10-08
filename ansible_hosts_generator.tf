9966bfdf8b295401d55f95e0bb75fb308bedcae3
Vasilii <mr_tomat@mail.ru>
2024-10-08 17:39:01 +0300

    ansible host generator updated

--- ansible_hosts_generator.tf
+++ ansible_hosts_generator.tf
@@ -1,5 +1,11 @@
 resource "local_file" "ansible_ini_inventory" {
-  content = join("\n", [for instance in yandex_compute_instance.vm :
-  "${instance.name} ansible_host=${instance.network_interface.0.nat_ip_address} ansible_user=ubuntu"])
-  filename = "${path.module}/hosts.ini"
+  content = <<EOF
+[all:vars]
+ansible_user=ubuntu
+
+[all]
+${join("\n", [for instance in yandex_compute_instance.vm :
+"${instance.name} ansible_host=${instance.network_interface.0.nat_ip_address}"])}
+EOF
+filename = "${path.module}/hosts.ini"
 }
